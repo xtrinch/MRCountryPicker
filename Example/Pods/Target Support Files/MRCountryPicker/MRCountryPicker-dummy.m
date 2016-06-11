287bdc8b8797ff6849da45444316e7662a83147c
Mojca Rojko <mojca.rojko@gmail.com>
2016-06-11 15:08:20 +0200

    Initial commit

---
+++ Example/Pods/Target Support Files/MRCountryPicker/MRCountryPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MRCountryPicker : NSObject
+@end
+@implementation PodsDummy_MRCountryPicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-MRCountryPicker_Tests/Pods-MRCountryPicker_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MRCountryPicker_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MRCountryPicker_TestsVersionString[];
+
